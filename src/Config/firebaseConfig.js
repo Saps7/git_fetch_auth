6f0b7da50230889a850bbaad417f564ec33d550c
saptarshi <saptarshisg.2000@gmail.com>
2020-08-11 00:40:14 +0530

    Completed without extra designs

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,11 @@
+export default {
+
+    apiKey: "AIzaSyBdSv4aVU2dEFiidaghCpyH0FWQhhNmg-s",
+    authDomain: "mygitapp-e16bb.firebaseapp.com",
+    databaseURL: "https://mygitapp-e16bb.firebaseio.com",
+    projectId: "mygitapp-e16bb",
+    storageBucket: "mygitapp-e16bb.appspot.com",
+    messagingSenderId: "732937105866",
+    appId: "1:732937105866:web:0ea1b413e124a6c484df33",
+    measurementId: "G-LF9PKM6BN1"
+};
